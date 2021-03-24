12db898dd237ed7538487e8e92267472c37acfa6
Daniel <daniel.n10@hotmail.com>
2021-03-24 09:11:07 -0300

    Create dropdown menu

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -11,3 +12,4 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
