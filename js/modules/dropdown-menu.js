1e86de9815869dad28aa893541cef73d29021cda
Daniel <daniel.n10@hotmail.com>
2021-03-24 20:44:45 -0300

    Create outside click

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,16 +1,21 @@
-export default function initDropdownMenu () {
+import outsideClick from './outsideclick.js';
 
+export default function initDropdownMenu () {
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+  dropdownMenus.forEach((menu) => {
+    ['touchstart', 'click'].forEach(userEvent => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    });
+  };
 }
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-dropdownMenus.forEach((menu) => {
-  ['touchstart', 'click'].forEach(userEvent => {
-    menu.addEventListener(userEvent, handleClick);
-  });
-});
-
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.toggle('active');
-};
