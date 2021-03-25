19f9ecfe99690c60822bb9d5baa88dbe39d3b09b
Daniel <daniel.n10@hotmail.com>
2021-03-24 21:34:49 -0300

    Create mobile menu

--- js/modules/outsideclick.js
+++ js/modules/outsideclick.js
@@ -3,9 +3,9 @@ export default function outsideClick(element, events, callback) {
   const outside = 'data-outside';
 
   if (!element.hasAttribute(outside)) {
-    events.ForEach(userEvent => {
-      html.addEventListener(userEvent, handleOutsideClick);
-    })
+    events.forEach(userEvent => {
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
+    });
     element.setAttribute(outside, '');
   }
 
