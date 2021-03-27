fe5c221c4870fe26a1e15628b6adc1b614e00162
Daniel <daniel.n10@hotmail.com>
2021-03-27 16:14:21 -0300

    Create fetch animals and bitcoins

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,5 +18,5 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
