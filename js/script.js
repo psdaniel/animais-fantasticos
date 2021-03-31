6f10a4a34825e6a1401f8584850928eac2d249b0
Daniel <daniel.n10@hotmail.com>
2021-03-31 09:22:59 -0300

    refactor tooltip

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import Accordion from './modules/accordion.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import TabNav from './modules/tab-nav.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
@@ -26,7 +26,9 @@ const modal = new Modal(
 );
 modal.init();
 
-initTooltip();
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
